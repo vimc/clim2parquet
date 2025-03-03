8eb1c6507d39fa6c6ad0d6849b9b64e747506e66
Pratik Gupte <pratikgupte16@gmail.com>
2025-03-03 17:06:16 +0000

    Initial set of climate data conversion funcs

---
+++ src/clim2parquet/__init__.py
@@ -0,0 +1,87 @@
+# SPDX-FileCopyrightText: 2025-present Imperial College of Science and Technology
+#
+# SPDX-License-Identifier: MIT
+
+from . import tools
+
+import os
+
+
+def chirps_to_parquet(dir: str, admin_level: int, to: str, gadm_version: str = "v410"):
+    """
+    Convert country CHIRPS data to a Parquet file.
+
+    Parameters
+    ----------
+    dir : str
+        Path to the data directory.
+    admin_level : int
+        GADM admin level as a single integer.
+    to : str
+        Path and filename to output the data.
+    gadm_version : str
+        GADM version as a string. Default is "v410" for v4.1.0.
+    """
+    data_source = "CHIRPS"
+    files = tools._find_clim_files(dir, data_source, admin_level, gadm_version)
+
+    if len(files) == 0:
+        print(f"Warning: Found no CHIRPS files under `{dir}`!")
+        return
+
+    tools._files_to_parquet(files, to)
+
+
+def era5mean_to_parquet(
+    dir: str, admin_level: int, to: str, gadm_version: str = "v410"
+):
+    """
+    Convert country CHIRPS data to a Parquet file.
+
+    Parameters
+    ----------
+    dir : str
+        Path to the data directory.
+    admin_level : int
+        GADM admin level as a single integer.
+    to : str
+        Path and filename to output the data.
+    gadm_version : str
+        GADM version as a string. Default is "v410" for v4.1.0.
+    """
+    data_source = "ERA5_Land_2m_temperature"
+    files = tools._find_clim_files(dir, data_source, admin_level, gadm_version)
+    exclude_pattern = "day"
+    files = [f for f in files if exclude_pattern not in os.path.basename(f)]
+
+    if len(files) == 0:
+        print(f"Warning: Found no ERA5 mean temp files under `{dir}`!")
+        return
+
+    tools._files_to_parquet(files, to)
+
+
+def clim_to_parquet(
+    dir: str, admin_level: int, dir_to: str, gadm_version: str = "v410"
+):
+    """
+    Convert country climate data to a Parquet file.
+
+    Parameters
+    ----------
+    dir : str
+        Path to the data directory.
+    dir_to: str
+        Path to the output directory.
+    admin_level : int
+        GADM admin level as a single integer.
+    gadm_version : str
+        GADM version as a string. Default is "v410" for v4.1.0.
+    """
+
+    # file names
+    chirps_file = "chirps_admin_" + str(admin_level) + ".parquet"
+    era5mean_file = "era5mean_admin_" + str(admin_level) + ".parquet"
+
+    chirps_to_parquet(dir, admin_level, chirps_file, gadm_version)
+    era5mean_to_parquet(dir, admin_level, era5mean_file, gadm_version)
