f5db1336acd654808eea4eae11e6bf7d10e0b4eb
Pratik Gupte <pratikgupte16@gmail.com>
2025-03-04 14:21:54 +0000

    Add functions for more data sources

--- src/clim2parquet/__init__.py
+++ src/clim2parquet/__init__.py
@@ -61,6 +61,135 @@ def era5mean_to_parquet(
     tools._files_to_parquet(files, to)
 
 
+def era5min_to_parquet(dir: str, admin_level: int, to: str, gadm_version: str = "v410"):
+    """
+    Convert country ERA5 land 2m daily minimum air temperature data to a Parquet
+    file.
+
+    Parameters
+    ----------
+    dir : str
+        Path to the data directory.
+    admin_level : int
+        GADM admin level as a single integer.
+    to : str
+        Path and filename to output the data.
+    gadm_version : str
+        GADM version as a string. Default is "v410" for v4.1.0.
+    """
+    data_source = "ERA5_Land_2m_temperature_daymin"
+    files = tools._find_clim_files(dir, data_source, admin_level, gadm_version)
+
+    if len(files) == 0:
+        print(f"Warning: Found no ERA5 min. temp. files under `{dir}`!")
+        return
+
+    tools._files_to_parquet(files, to)
+
+
+def era5max_to_parquet(dir: str, admin_level: int, to: str, gadm_version: str = "v410"):
+    """
+    Convert country ERA5 land 2m daily maximum air temperature data to a Parquet
+    file.
+
+    Parameters
+    ----------
+    dir : str
+        Path to the data directory.
+    admin_level : int
+        GADM admin level as a single integer.
+    to : str
+        Path and filename to output the data.
+    gadm_version : str
+        GADM version as a string. Default is "v410" for v4.1.0.
+    """
+    data_source = "ERA5_Land_2m_temperature_daymax"
+    files = tools._find_clim_files(dir, data_source, admin_level, gadm_version)
+
+    if len(files) == 0:
+        print(f"Warning: Found no ERA5 max. temp. files under `{dir}`!")
+        return
+
+    tools._files_to_parquet(files, to)
+
+
+def era5rh_to_parquet(dir: str, admin_level: int, to: str, gadm_version: str = "v410"):
+    """
+    Convert country ERA5 land 2m daily relative humidity data to a Parquet file.
+
+    Parameters
+    ----------
+    dir : str
+        Path to the data directory.
+    admin_level : int
+        GADM admin level as a single integer.
+    to : str
+        Path and filename to output the data.
+    gadm_version : str
+        GADM version as a string. Default is "v410" for v4.1.0.
+    """
+    data_source = "ERA5_Land_relative_humidity"
+    files = tools._find_clim_files(dir, data_source, admin_level, gadm_version)
+
+    if len(files) == 0:
+        print(f"Warning: Found no ERA5 RH files under `{dir}`!")
+        return
+
+    tools._files_to_parquet(files, to)
+
+
+def era5sh_to_parquet(dir: str, admin_level: int, to: str, gadm_version: str = "v410"):
+    """
+    Convert country ERA5 land 2m daily specific humidity data to a Parquet file.
+
+    Parameters
+    ----------
+    dir : str
+        Path to the data directory.
+    admin_level : int
+        GADM admin level as a single integer.
+    to : str
+        Path and filename to output the data.
+    gadm_version : str
+        GADM version as a string. Default is "v410" for v4.1.0.
+    """
+    data_source = "ERA5_Land_specific_humidity"
+    files = tools._find_clim_files(dir, data_source, admin_level, gadm_version)
+
+    if len(files) == 0:
+        print(f"Warning: Found no ERA5 SH files under `{dir}`!")
+        return
+
+    tools._files_to_parquet(files, to)
+
+
+def persiann_to_parquet(
+    dir: str, admin_level: int, to: str, gadm_version: str = "v410"
+):
+    """
+    Convert country ERA5 land 2m daily specific humidity data to a Parquet file.
+
+    Parameters
+    ----------
+    dir : str
+        Path to the data directory.
+    admin_level : int
+        GADM admin level as a single integer.
+    to : str
+        Path and filename to output the data.
+    gadm_version : str
+        GADM version as a string. Default is "v410" for v4.1.0.
+    """
+    data_source = "PERSIANN"
+    files = tools._find_clim_files(dir, data_source, admin_level, gadm_version)
+
+    if len(files) == 0:
+        print(f"Warning: Found no PERSIANN files under `{dir}`!")
+        return
+
+    tools._files_to_parquet(files, to)
+
+
 def clim_to_parquet(
     dir: str, admin_level: int, dir_to: str, gadm_version: str = "v410"
 ):
