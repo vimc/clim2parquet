bc2336371b8db2ac62d87af1e54a4ba16c930be2
Pratik Gupte <pratikgupte16@gmail.com>
2025-03-05 14:41:44 +0000

    Update test files

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,15 @@
 # SPDX-FileCopyrightText: 2025-present Imperial College of Science and Technology
 #
 # SPDX-License-Identifier: MIT
+
+import clim2parquet
+
+
+def test_data_names():
+    """
+    Test that data names are returned.
+    """
+    data_names = clim2parquet.get_data_names()
+
+    assert isinstance(data_names, list)
+    assert len(data_names) == 7
