5accd2e96de946b1bc8068d1bcd30d252ff0ffcc
Pratik Gupte <pratikgupte16@gmail.com>
2025-03-06 13:10:28 +0000

    Move test for data info file

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,14 +2,4 @@
 #
 # SPDX-License-Identifier: MIT
 
-"""Test function to get data information from package data."""
-
-import clim2parquet
-
-
-def test_data_names() -> None:
-    """Test that data names are returned."""
-    data_names = clim2parquet.get_data_names()
-
-    assert isinstance(data_names, list)
-    assert len(data_names) == 7
+"""Tests for clim2parquet."""
